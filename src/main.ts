692b524a1091159822c25224fb2a254b65227d33
muhhammedd <148395547+muhhammedd@users.noreply.github.com>
2026-01-07 11:08:28 -0500

    feat: integrate swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './common/interfaces/express.interface';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { TenantInterceptor } from './common/interceptors/tenant.interceptor';
@@ -21,6 +22,16 @@ async function bootstrap() {
     new TenantInterceptor(),
   );
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Shipex API')
+    .setDescription('The Shipex Shipping Management Platform API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
