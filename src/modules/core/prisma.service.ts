f5a8eb130002eecfe98cea2201470d05fe8b6b79
muhhammedd <148395547+muhhammedd@users.noreply.github.com>
2026-01-09 09:09:42 -0500

    fix: refactor PrismaService to use composition for better compatibility with Prisma v7

--- src/modules/core/prisma.service.ts
+++ src/modules/core/prisma.service.ts
@@ -2,19 +2,69 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private prismaClient: PrismaClient;
+
   constructor() {
-    super();
+    this.prismaClient = new PrismaClient({
+      errorFormat: 'pretty',
+    });
+  }
+
+  get client(): PrismaClient {
+    return this.prismaClient;
+  }
+
+  // Proxy methods to maintain backward compatibility
+  get order() {
+    return this.prismaClient.order;
+  }
+
+  get user() {
+    return this.prismaClient.user;
+  }
+
+  get tenant() {
+    return this.prismaClient.tenant;
+  }
+
+  get merchantProfile() {
+    return this.prismaClient.merchantProfile;
+  }
+
+  get courierProfile() {
+    return this.prismaClient.courierProfile;
+  }
+
+  get uploadedFile() {
+    return this.prismaClient.uploadedFile;
+  }
+
+  get notification() {
+    return this.prismaClient.notification;
+  }
+
+  get orderHistory() {
+    return this.prismaClient.orderHistory;
+  }
+
+  async $connect() {
+    return this.prismaClient.$connect();
+  }
+
+  async $disconnect() {
+    return this.prismaClient.$disconnect();
+  }
+
+  async $transaction(callback: (tx: PrismaClient) => Promise<any>) {
+    return this.prismaClient.$transaction(callback);
   }
 
   async onModuleInit() {
-    await this.$connect();
+    await this.prismaClient.$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.prismaClient.$disconnect();
   }
 }
